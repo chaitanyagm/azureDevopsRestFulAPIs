2a2593f25c2eaa33170d45576d2be724901eaefd
chaitanyagm <55421191+chaitanyagm@users.noreply.github.com>
2019-11-06 18:09:03 +1100

    Update AzureDevopsREstfulAPI_cmds.sh

--- AzureDevopsREstfulAPI_cmds.sh
+++ AzureDevopsREstfulAPI_cmds.sh
@@ -1,3 +1,20 @@
 
 
 curl -u yourAzDevopsUsername:yourAzureAccessToken https://dev.azure.com/yourOrganization/_apis/projects?api-version=2.0
+
+# {
+    "count":1,
+    "value":[
+              {
+                "id":"dff8d9-8493-398t-g686-kjfksldjfsdkl",
+                "name":"test",
+                "url":"https://dev.azure.com/organizationname/_apis/projects/dff8d9-8493-398t-g686-kjfksldjfsdkl",
+                "state":"wellFormed",
+                "revision":12,
+                "visibility":"private",
+                "lastUpdateTime":"2019-10-31T06:25:08.94Z"
+                }
+             ]
+  }
+   
+
